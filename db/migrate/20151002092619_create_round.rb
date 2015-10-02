d58120b81e9773aab813e7b5886968b75adde255
André & Amy Liao <pair+ajessee.amyhliao@devbootcamp.com>
2015-10-02 17:22:05 -0400

    Add next card method in round

--- db/migrate/20151002092619_create_round.rb
+++ db/migrate/20151002092619_create_round.rb
@@ -1,6 +1,6 @@
 class CreateRound < ActiveRecord::Migration
   def change
-  	  	create_table :rounds do |t|
+  	 create_table :rounds do |t|
   		t.belongs_to :user, index: true
   		t.belongs_to :deck
 
