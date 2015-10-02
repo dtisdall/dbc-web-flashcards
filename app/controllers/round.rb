e0fba688402265a8a5f388addca1e8452fde5e85
André & Amy Liao <pair+ajessee.amyhliao@devbootcamp.com>
2015-10-02 17:22:05 -0400

    Add next card method in round

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,3 +1,18 @@
+# index: GET /rounds  (display a list of all rounds)
+
+# new: GET /rounds/new (return an HTML form for creating a new round)
+
+# create: POST  /rounds (create a new round)
+
+# show: GET /rounds/1 (display a specific round)
+
+# edit: GET /rounds/1/edit (return an HTML form for editing a round)
+
+# update: PUT /rounds/1 (update a specific round)
+
+# destroy: DELETE  /rounds/1 (delete a specific round)
+
+
 post '/round' do
 	#This is supposed to start a new round, will post a hash params{new_round{user_id: 1, deck_id: 1}}
   p params.to_s
--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,11 @@ class Round < ActiveRecord::Base
   belongs_to :user
   has_many :guesses
   belongs_to :deck
+
+  def next_card
+    correctly_guessed_cards = self.guesses.where(correct: true).map{|guess| guess.card}
+    all_cards = self.deck.cards
+    cards_without_correct_answers = all_cards - correctly_guessed_cards
+    cards_without_correct_answers.sample
+  end
 end
